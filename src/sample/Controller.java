98146ceee6e1039c1abc6626885986291770d85b
Ismar <izdralovic2@etf.unsa.ba>
2021-03-16 15:23:57 +0100

    Prvi commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
+public class Controller {
+    public void Submit(ActionEvent actionEvent) {
+        System.out.println("Hello World");
+
+    }
+    public GridPane hellow;
+}
